688d86638ed6ff2138fcba8695245e3e68c07593
EC2 Default User <ec2-user@ip-172-31-2-26.ap-northeast-1.compute.internal>
2016-01-28 02:51:17 +0000

    add a script for the Tornado Server

---
+++ tornado.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Tornado Server for Catlog
+#
+workdir=/home/ec2-user/online-parser
+ 
+start() {
+    cd $workdir
+    /usr/bin/python /home/ec2-user/online-parser/parser.py --port="$1" --debug=Fasle --log_file_prefix=/home/ec2-user/online-parser/logs/tornado-"$1".log &
+    echo "Server started."
+}
+ 
+stop() {
+    pid=`ps -ef | grep '[p]ython /home/ec2-user/online-parser/parser --port='"$1" | awk '{ print $2 }'`
+    echo $pid
+    kill $pid
+    sleep 2
+    echo "Server killed."
+}
+ 
+case "$1" in
+  start)
+    start "$2"
+    ;;
+  stop)
+    stop "$2"  
+    ;;
+  restart)
+    stop "$2"
+    start "$2"
+    ;;
+  *)
+    echo "Usage: ./tornado.sh {start|stop|restart}"
+    exit 1
+esac
+exit 0
